1f533b2022427ecff60dd9631da7d8475229920f
Rodrigo Chacon <rochacon@gmail.com>
2013-06-26 06:19:15 -0300

    NGINX dont support includes inside upstream blocks

--- elastic_nginx/app.py
+++ elastic_nginx/app.py
@@ -13,13 +13,16 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_REGION = os.environ.get('AWS_REGION', 'us-east-1')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 TOPIC_ARN = os.environ['TOPIC_ARN']
-UPSTREAM_CONF_TEMPLATE = os.environ.get('UPSTREAM_CONF_TEMPLATE', '/etc/nginx/conf.d/upstreams/api/%s.upstream')
+UPSTREAM_NAME = os.environ.get('UPSTREAM_NAME', 'backends')
+UPSTREAM_CONF_FILE = os.environ.get('UPSTREAM_CONF_FILE', '/etc/nginx/conf.d/upstreams/api.upstreams')
+UPSTREAM_CONFS_PATH = os.environ.get('UPSTREAM_CONFS_PATH', '/etc/nginx/conf.d/upstreams/api')
+UPSTREAM_CONF_EXTENSION = os.environ.get('UPSTREAM_CONF_EXTENSION', '.upstream')
 
 
 def add_instance_to_lb(instance_id):
     print 'Adding %s to LB' % instance_id
 
-    conf = UPSTREAM_CONF_TEMPLATE % instance_id
+    conf = os.path.join(UPSTREAM_CONFS_PATH, instance_id + UPSTREAM_CONF_EXTENSION)
 
     if os.path.exists(conf):
         return False
@@ -32,18 +35,33 @@ def add_instance_to_lb(instance_id):
     with open(conf, 'wb') as f:
         f.write('server %s:80 max_fails=3 fail_timeout=60s;' % instance.private_ip_address)
 
-    nginx_reload()
+    nginx_reconfigure()
 
 
 def rm_instance_to_lb(instance_id):
     print 'Removing %s from LB' % instance_id
 
-    conf = UPSTREAM_CONF_TEMPLATE % instance_id
+    conf = os.path.join(UPSTREAM_CONFS_PATH, instance_id + UPSTREAM_CONF_EXTENSION)
 
     if os.path.exists(conf):
         os.unlink(conf)
 
-    nginx_reload()
+    nginx_reconfigure()
+
+
+def nginx_reconfigure():
+    print 'Reconfiguring NGINX upstream %s' % UPSTREAM_NAME
+    upstream = 'upstream %s {\n' % UPSTREAM_NAME
+
+    for server in os.listdir(UPSTREAM_CONFS_PATH):
+        if server.endswith(UPSTREAM_CONF_EXTENSION):
+            with open(os.path.join(UPSTREAM_CONFS_PATH, server), 'rb') as f:
+                upstream += '  %s\n' % f.read()
+
+    upstream += '}\n'
+
+    with open(UPSTREAM_CONF_FILE, 'wb') as f:
+        f.write(upstream)
 
 
 def nginx_reload():
